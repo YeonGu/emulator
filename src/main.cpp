b1737b5e9727d3ec3aa48b85824fd41c241d6a87
YeonGu <1277941818@qq.com>
2023-09-11 22:12:55 +0800

    Add: apu, audio test

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@
 #include "input_manager.h"
 #include "mmu.h"
 #include "nes.h"
+#include <chrono>
+#include <cmath>
 #include <cpu.h>
 #include <cstdio>
 #include <cstdlib>
@@ -17,24 +19,73 @@
 void  init_memory();
 char *rom_path = nullptr;
 
+const int AUDIO_AMP         = 28000;
+const int AUDIO_SAMPLE_RATE = 44100;
+const int AUDIO_DURATION    = 2;
+void      sdl_audio_callback( void *userdata, Uint8 *stream, int len )
+{
+    //    Sint16 *samples     = reinterpret_cast<Sint16 *>( stream );
+    //    int     sampleCount = len / sizeof( Sint16 );
+    //    double  freq        = 440.0; // 频率为440Hz
+    //
+    //    for ( int i = 0; i < sampleCount; ++i )
+    //    {
+    //        double time  = static_cast<double>( i ) / AUDIO_SAMPLE_RATE;
+    //        double value = AUDIO_AMP * sin( 2.0 * M_PI * freq * time );
+    //        samples[ i ] = static_cast<Sint16>( value );
+    //    }
+    Sint16 *audio  = reinterpret_cast<Sint16 *>( stream );
+    int     length = len / sizeof( Sint16 );
+
+    auto     now     = std::chrono::system_clock::now().time_since_epoch();
+    uint64_t time_ms = std::chrono::duration_cast<std::chrono::microseconds>( now ).count();
+    double   t       = static_cast<double>( time_ms ) / 1000;
+
+    double increment = 2.0 * M_PI * 440 / AUDIO_SAMPLE_RATE;
+    double angle     = 0.0;
+
+    for ( int i = 0; i < length; i++ )
+    {
+        double time = static_cast<double>( i ) / AUDIO_SAMPLE_RATE + t;
+        audio[ i ]  = static_cast<Sint16>( AUDIO_AMP * sin( t * 2.0 * 3.14159 * 440 ) );
+        angle += increment;
+    }
+}
+
 int main( int argc, char *argv[] )
 {
     if ( argc >= 2 )
         rom_path = argv[ 1 ];
-    if ( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK ) < 0 )
+
+    SDL_setenv( "SDL_AUDIODRIVER", "directsound", 0 );
+    if ( SDL_Init( SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_AUDIO ) < 0 )
     {
         printf( "SDL初始化失败: %s\n", SDL_GetError() );
         return 1;
     }
+    SDL_AudioSpec desiredSpec, obtainedSpec;
+    desiredSpec.freq     = AUDIO_SAMPLE_RATE;
+    desiredSpec.format   = AUDIO_S16;
+    desiredSpec.channels = 1;
+    desiredSpec.samples  = 2048;
+    desiredSpec.callback = sdl_audio_callback;
+    desiredSpec.userdata = nullptr;
+    //        SDL_AudioDriverName( "directsound" );
+
+    if ( SDL_OpenAudio( &desiredSpec, &obtainedSpec ) != 0 )
+    {
+        std::cout << "Failed to open audio device: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+    SDL_PauseAudio( 0 );
+    //    SDL_Delay( 2000 );
 
     init_mmu();
     init_input_manager();
 
     read_rom_mapper( argc, argv ); // Load ROM and INTERRUPT VECTORS ( nes_rom.c -> memory.c ); Init CPU cpu_memory_mapper
                                    // read_rom() -> read_info() -> init_cpu_mapper()
-
     init_memory();
-
     init_cpu(); // Init CPU (RESET vector). CPU Power up state...
 
     nes_mainloop();
